bb53c4149b932269221b09990bc5d0799d2c69ac
matong <malutong@yichengsen.com>
2018-09-06 13:29:49 +0800

    V:1.0.3--适配器模式

---
+++ design/Adapter.php
@@ -0,0 +1,54 @@
+<?php
+interface database{
+    function connect();
+    function select();
+    function add();
+    function del();
+}
+class MySql implements database {
+    public function connect()
+    {
+        echo __CLASS__."链接";
+    }
+    public function select()
+    {
+        echo __CLASS__."查询";
+    }
+    public function add()
+    {
+        echo __CLASS__."添加";
+    }
+    public function del()
+    {
+        echo __CLASS__."删除";
+    }
+}
+class Mongodb implements database {
+    public function connect()
+    {
+        echo __CLASS__."链接<br/>";
+    }
+    public function select()
+    {
+        echo __CLASS__."查询<br/>";
+    }
+    public function add()
+    {
+        echo __CLASS__."添加<br/>";
+    }
+    public function del()
+    {
+        echo __CLASS__."删除<br/>";
+    }
+}
+class Adapter{
+    public function index()
+    {
+        $db = new MySql();
+        $db->connect();
+        $db->del();
+        $db->select();
+        $db->add();
+    }
+}
+(new Adapter())->index();
